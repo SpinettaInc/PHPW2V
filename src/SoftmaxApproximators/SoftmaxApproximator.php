53861c6513fa3acd1802edd211756e76df47e850
Eleo Basili <eleonel.basili@gmail.com>
2023-06-11 10:54:36 +0900

    Code refactoring.

--- src/SoftmaxApproximators/SoftmaxApproximator.php
+++ src/SoftmaxApproximators/SoftmaxApproximator.php
@@ -5,31 +5,38 @@ namespace PHPW2V\SoftmaxApproximators;
 use PHPW2V\Word2Vec;
 use Tensor\Vector;
 
-interface SoftmaxApproximator
-{
-    /**
-     * Create sampling structure used in pair train approximation.
-     *
-     * @param \PHPW2V\Word2Vec $word2vec
-     * @throws \InvalidArgumentException
-     */
-    public function structureSampling(Word2Vec $word2vec) : void;
+/**
+ * Interface for softmax approximators.
+ */
+interface SoftmaxApproximator {
 
-    /**
-     * Return the word indices used to update the hidden layer.
-     *
-     * @param mixed[] $predictWord
-     * @return int[]
-     */
-    public function wordIndices(array $predictWord) : array;
+  /**
+   * Create sampling structure used in pair train approximation.
+   *
+   * @param \PHPW2V\Word2Vec $word2vec
+   *
+   * @throws \InvalidArgumentException.
+   */
+  public function structureSampling(Word2Vec $word2vec) : void;
 
-    /**
-     * Calculate the gradient descent.
-     *
-     * @param Vector $fa
-     * @param string $predictWord
-     * @param float $alpha
-     * @return Vector
-     */
-    public function gradientDescent(Vector $fa, string $predictWord, float $alpha) : Vector;
+  /**
+   * Return the word indices used to update the hidden layer.
+   *
+   * @param mixed[] $predictWord
+   *
+   * @return int[]
+   */
+  public function wordIndices(array $predictWord) : array;
+
+  /**
+   * Calculate the gradient descent.
+   *
+   * @param \Tensor\Vector $fa
+   * @param string $predictWord
+   * @param float $alpha
+   *
+   * @return \Tensor\Vector
+   */
+  public function gradientDescent(Vector $fa, string $predictWord, float $alpha) : Vector;
+
 }
